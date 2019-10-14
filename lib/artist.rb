c0eee2ac83d8313d22485e1ba2afa6d4f7316aac
Amy Nguyen <amy.nguyen730@gmail.com>
2019-10-14 14:05:58 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,27 @@
 class Artist
     attr_accessor :name
-    attr_reader :songs
   
-    @@song_count = 0
+    @@all = []
   
     def initialize(name)
       @name = name
-      @songs = []
+      @@all << self
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
     end
   
     def add_song(song)
-        self.songs << song
         song.artist = self
-        @@song_count +=1
     end
   
     def add_song_by_name(name)
       song = Song.new(name)
-      self.songs << song
       song.artist = self
-      @@song_count +=1
     end
   
     def self.song_count
-      @@song_count
+      Song.all.count
     end
   end
